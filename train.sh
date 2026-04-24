785774c4047d524103a44f194b4d6a5241d48c77
Vyshnav <vyshnav20157@iiitd.ac.in>
2026-04-24 10:32:29 +0100

    Sec B complete.

--- train.sh
+++ train.sh
@@ -3,14 +3,15 @@
 STUDENT_ID=zv00033 STUDENT_NAME="Vyshnav Vijay" python main.py \
 -s veri \
 -t veri \
--a vgg16 \
+-a resnet18 \
 --root src/datasets \
 --height 224 \
 --width 224 \
 --optim amsgrad \
 --lr 0.0003 \
+--random-erase \
 --max-epoch 10 \
 --stepsize 20 40 \
 --train-batch-size 64 \
 --test-batch-size 100 \
---save-dir logs/vgg16-veri
+--save-dir logs/resnet18-randomerase
